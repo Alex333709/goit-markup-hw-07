0d42b88f0ad7185a717a34a10b4aa6fca7b1faa6
Oleksandr Voloshun <110338309+Alex333709@users.noreply.github.com>
2023-04-18 21:36:06 +0200

    Initial commit

---
+++ js/modal.js
@@ -0,0 +1,24 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+  refs.openModalBtn.forEach(Element => {
+    Element.addEventListener('click', toggleModal);
+  });
+
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  //* Закривання по кліку на бекдроп
+  //   refs.modal.addEventListener('click', removeModal);
+  //   function removeModal(e) {
+  //     if (e.target === e.currentTarget) {
+  //       refs.modal.classList.add('is-hidden');
+  //     }
+  //   }
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
+  }
+})();
